29ebff5286134ff0b51a31e8d227764294182c9b
sleroy <sylvain.leroy@tocea.com>
2017-04-08 10:41:51 +0200

    fixed indentation

--- test/index.js
+++ test/index.js
@@ -5,69 +5,67 @@ var Hexo = require('hexo');
 var cheerio = require('cheerio');
 
 describe('Sitemap generator', function() {
-    var hexo = new Hexo(__dirname, { silent: true });
-    var Post = hexo.model('Post');
-    var generator = require('../lib/generator').bind(hexo);
-    var sitemapTmpl = require('../lib/template')();
-    var posts;
-    var locals;
+  var hexo = new Hexo(__dirname, { silent: true });
+  var Post = hexo.model('Post');
+  var generator = require('../lib/generator').bind(hexo);
+  var sitemapTmpl = require('../lib/template')();
+  var posts;
+  var locals;
 
-    before(function() {
-        return hexo.init().then(function() {
-            return Post.insert([
-                { source: 'foo', slug: 'foo', updated: 1e8 },
-                { source: 'bar', slug: 'bar', updated: 1e8 + 1 },
-                { source: 'baz', slug: 'baz', updated: 1e8 - 1 }
+  before(function() {
+    return hexo.init().then(function() {
+      return Post.insert([
+          { source: 'foo', slug: 'foo', updated: 1e8 },
+          { source: 'bar', slug: 'bar', updated: 1e8 + 1 },
+          { source: 'baz', slug: 'baz', updated: 1e8 - 1 }
             ]).then(function(data) {
-                posts = Post.sort('-updated');
-                locals = hexo.locals.toObject();
+              posts = Post.sort('-updated');
+              locals = hexo.locals.toObject();
             });
-        });
+    });
+  });
+
+  it('default', function() {
+    hexo.config.sitemap = {
+      path: 'sitemap.xml'
+    };
+
+    var result = generator(locals);
+
+    result.path.should.eql('sitemap.xml');
+    result.data.should.eql(sitemapTmpl.render({
+      config: hexo.config,
+      posts: posts.toArray()
+    }));
+
+    var $ = cheerio.load(result.data);
+
+    $('urlset').find('url').each(function(i) {
+      $(this).children('loc').text().should.eql(posts.eq(i).permalink);
+      $(this).children('lastmod').text().should.eql(posts.eq(i).updated.toISOString());
+    });
+  });
+
+  describe('skip_render', function() {
+    it('array', function() {
+      hexo.config.skip_render = ['foo'];
+
+      var result = generator(locals);
+      result.data.should.not.contain('foo');
     });
 
-    it('default', function() {
-        hexo.config.sitemap = {
-            path: 'sitemap.xml'
-        };
+    it('string', function() {
+      hexo.config.skip_render = 'bar';
 
-        var result = generator(locals);
-
-        result.path.should.eql('sitemap.xml');
-        result.data.should.eql(sitemapTmpl.render({
-            config: hexo.config,
-            posts: posts.toArray()
-        }));
-
-        var $ = cheerio.load(result.data);
-
-        $('urlset').find('url').each(function(i) {
-            $(this).children('loc').text().should.eql(posts.eq(i).permalink);
-            $(this).children('lastmod').text().should.eql(posts.eq(i).updated.toISOString());
-        });
+      var result = generator(locals);
+      result.data.should.not.contain('bar');
     });
 
+    it('off', function() {
+      hexo.config.skip_render = null;
 
-
-    describe('skip_render', function() {
-        it('array', function() {
-            hexo.config.skip_render = ['foo'];
-
-            var result = generator(locals);
-            result.data.should.not.contain('foo');
-        });
-
-        it('string', function() {
-            hexo.config.skip_render = 'bar';
-
-            var result = generator(locals);
-            result.data.should.not.contain('bar');
-        });
-
-        it('off', function() {
-            hexo.config.skip_render = null;
-
-            var result = generator(locals);
-            result.should.be.ok;
-        });
+      var result = generator(locals);
+      result.should.be.ok;
     });
+  });
 });
--- test/utils-test.js
+++ test/utils-test.js
@@ -4,28 +4,28 @@ var should = require('chai').should(); // eslint-disable-line
 var assert = require('assert');
 
 describe('Hexo Sitemap Generator utils', function() {
-    var utils = require('../lib/utils');
+  var utils = require('../lib/utils');
 
-    describe('matching_function', function() {
-        it('no_pattern', function() {
-            // Everything is accepted
-            assert.equal(false, utils.isMatch("page.html"));
-            assert.equal(false, utils.isMatch("page.js"));
-            assert.equal(false, utils.isMatch("page.css"));
-        });
-
-        it('single_pattern', function() {
-            // Only CSS is filtered
-            assert.equal(false, utils.isMatch("page.html", "*.css"), "Should not be filtered");
-            assert.equal(false, utils.isMatch("page.js", "*.css"), "Should not be filtered");
-            assert.equal(true, utils.isMatch("page.css", "*.css", "Should be filtered"));
-        });
-
-        it('empty_array_pattern', function() {
-            // Everything is accepted
-            assert.equal(false, utils.isMatch("page.html", []));
-            assert.equal(false, utils.isMatch("page.js", []));
-            assert.equal(false, utils.isMatch("page.css", []));
-        });
+  describe('matching_function', function() {
+    it('no_pattern', function() {
+      // Everything is accepted
+      assert.equal(false, utils.isMatch('page.html'));
+      assert.equal(false, utils.isMatch('page.js'));
+      assert.equal(false, utils.isMatch('page.css'));
     });
+
+    it('single_pattern', function() {
+      // Only CSS is filtered
+      assert.equal(false, utils.isMatch('page.html', '*.css'), 'Should not be filtered');
+      assert.equal(false, utils.isMatch('page.js', '*.css'), 'Should not be filtered');
+      assert.equal(true, utils.isMatch('page.css', '*.css', 'Should be filtered'));
+    });
+
+    it('empty_array_pattern', function() {
+      // Everything is accepted
+      assert.equal(false, utils.isMatch('page.html', []));
+      assert.equal(false, utils.isMatch('page.js', []));
+      assert.equal(false, utils.isMatch('page.css', []));
+    });
+  });
 });
